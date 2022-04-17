37984aca1b5cdf0b311078519a90ccaf0fcfc605
Suraj Pathak <pathaksuraj136@gmail.com>
2022-04-17 19:28:04 +0530

    updated

--- suraj_backend/src/configs/db.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports = () => {
-  return mongoose.connect("mongodb://suraj:suraj_4321@cluster0-shard-00-00.qybgl.mongodb.net:27017,cluster0-shard-00-01.qybgl.mongodb.net:27017,cluster0-shard-00-02.qybgl.mongodb.net:27017/new_auth_sys?ssl=true&replicaSet=atlas-3tamaf-shard-0&authSource=admin&retryWrites=true&w=majority");
-};
